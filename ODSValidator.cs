147a25a4681047df9bd18a662ca38d9dcd16ea6f
Honza Vrátník <vratnja2@fel.cvut.cz>
2017-04-24 00:17:47 +0200

    Repository initialization

---
+++ ODSValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AODL.Document.SpreadsheetDocuments;
+using VerifilerCore;
+
+namespace VerifilerODF {
+	/// <summary>
+	/// This validation step is using the AODL library using the OpenOffice API.
+	/// 
+	/// The error code produced by this validation is Error.Corrupted.
+	/// </summary>
+	public class ODSValidator : FormatSpecificValidator {
+
+		public override int ErrorCode { get; set; } = Error.Corrupted;
+
+		public override void Setup() {
+			Name = "OpenDocument sheet files verification";
+			RelevantExtensions.Add(".ods");
+			Enable();
+		}
+
+		public override void ValidateFile(string file) {
+			try {
+				SpreadsheetDocument document = new SpreadsheetDocument();
+				document.Load(file);
+			} catch (Exception e) {
+				ReportAsError("File is corrupted: " + file + "; Message: " + e.Message);
+				GC.Collect();
+			}
+		}
+	}
+}
---
+++ ODTValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AODL.Document.TextDocuments;
+using VerifilerCore;
+
+namespace VerifilerODF {
+
+	/// <summary>
+	/// This validation step is using the AODL library using the OpenOffice API.
+	/// 
+	/// The error code produced by this validation is Error.Corrupted.
+	/// </summary>
+	public class ODTValidator : FormatSpecificValidator {
+
+		public override int ErrorCode { get; set; } = Error.Corrupted;
+
+		public override void Setup() {
+			Name = "OpenDocument text files verification";
+			RelevantExtensions.Add(".odt");
+			Enable();
+		}
+
+		public override void ValidateFile(string file) {
+			try {
+				TextDocument document = new TextDocument();
+				document.Load(file);
+			} catch (Exception e) {
+				ReportAsError("File is corrupted: " + file + "; Message: " + e.Message);
+				GC.Collect();
+			}
+		}
+	}
+}
